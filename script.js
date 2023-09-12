4e963983db42e61c4c4bb6535648331d9544d7d0
Hai Nguyen <nguyenxuanhai99@gmail.com>
2023-09-12 20:55:15 +0700

    latest files

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'dark.css'
+	}
+
+
+	if(mode == 'orange'){
+		document.getElementById('theme-style').href = 'orange.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
